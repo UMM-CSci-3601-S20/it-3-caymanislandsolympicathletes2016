f82cdc3904ee94fdee2d470ba1970253a5e1bcff
Joe Walbran <walbr037@morris.umn.edu>
2020-03-03 21:45:03 -0600

    Quote more variables in mongoseed.sh
    Now, the script should also work if you have spaces in the path to the `databases` directory.

--- database/mongoseed.sh
+++ database/mongoseed.sh
@@ -3,7 +3,7 @@
 seed_db="${MONGO_DB:-dev}"
 echo Dropping DB $seed_db
 mongo "$seed_db" --eval "db.dropDatabase()"
-for file in "$(dirname $BASH_SOURCE)"/seed/*.json; do
+for file in "$(dirname "$BASH_SOURCE")"/seed/*.json; do
   if [[ -f "$file" ]]; then
     echo Seeding $(basename "$file" ".json") from $file in DB $seed_db
     mongoimport --db="$seed_db" --collection="$(basename "$file" ".json")" --file="$file" --jsonArray
