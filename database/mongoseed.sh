86000587459b617c02d0d7283a42211a7d20ac47
Joe Walbran <walbr037@morris.umn.edu>
2020-03-03 18:37:17 -0600

    Fix bug in mongoseed.sh
    The script was breaking if there were no elements in the `database/seed/` directory. In that case, it would try to read from a file literally named `database/seed/*.json`, which of course does not exist.
    
    This commit fixes that error. Now, for each file in `database/seed/*.json`, the script will check to see if the file exists first. This catches the case where the script literally tries to seed `*.json`.

--- database/mongoseed.sh
+++ database/mongoseed.sh
@@ -4,6 +4,8 @@ SEED_DB=${MONGO_DB:-dev}
 echo Dropping DB $SEED_DB
 mongo $SEED_DB --eval "db.dropDatabase()"
 for file in $(dirname $BASH_SOURCE)/seed/*.json; do
-  echo Seeding $(basename "$file" ".json") from $file in DB $SEED_DB
-  mongoimport --db=$SEED_DB --collection=$(basename "$file" ".json") --file=$file --jsonArray
+  if [[ -f file ]]; then
+    echo Seeding $(basename "$file" ".json") from $file in DB $SEED_DB
+    mongoimport --db=$SEED_DB --collection=$(basename "$file" ".json") --file=$file --jsonArray
+  fi
 done
