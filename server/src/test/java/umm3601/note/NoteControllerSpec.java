6fd56551991a0ed5386268e9a6203809208dd2e9
Luke <burde041@morris.umn.edu>
2020-03-05 13:54:34 -0600

    Add NoteControllerSpec

---
+++ server/src/test/java/umm3601/note/NoteControllerSpec.java
@@ -0,0 +1,81 @@
+package umm3601.note;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.mockrunner.mock.web.MockHttpServletResponse;
+import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import umm3601.notes.NoteController;
+
+public class NoteControllerSpec {
+
+  MockHttpServletRequest mockReq = new MockHttpServletRequest();
+  MockHttpServletResponse mockRes = new MockHttpServletResponse();
+
+  private NoteController noteController;
+
+  static MongoClient mongoClient;
+  static MongoDatabase db;
+
+  static ObjectMapper jsonMapper = new ObjectMapper();
+
+  @BeforeAll
+  public static void setupAll() {
+    String mongoAddr = System.getenv().getOrDefault("MONGO_ADDR", "localhost");
+
+    mongoClient = MongoClients.create(
+    MongoClientSettings.builder()
+    .applyToClusterSettings(builder ->
+    builder.hosts(Arrays.asList(new ServerAddress(mongoAddr))))
+    .build());
+
+    db = mongoClient.getDatabase("test");
+  }
+
+  @BeforeEach
+  public void setupEach() throws IOException {
+
+    // Reset our mock request and response objects
+    mockReq.resetAll();
+    mockRes.resetAll();
+
+    // Setup database
+    MongoCollection<Document> noteDocuments = db.getCollection("notes");
+    noteDocuments.drop();
+    List<Document> testNotes = new ArrayList<>();
+    testNotes.add(Document.parse("{\n" +
+    "                    body: \"This is the first body\",\n" +
+    "                }"));
+    testNotes.add(Document.parse("{\n" +
+    "                    body: \"This is the second body\",\n" +
+    "                }"));
+    testNotes.add(Document.parse("{\n" +
+    "                    body: \"This is the third body\",\n" +
+    "                }"));
+
+    noteDocuments.insertMany(testNotes);
+
+    noteController = new NoteController(db);
+  }
+
+  @AfterAll
+  public static void teardown() {
+    db.drop();
+    mongoClient.close();
+  }
+}
