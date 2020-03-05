c37b79add9ba9ff6efb358579d57607c34e821e7
Joe Walbran <walbr037@morris.umn.edu>
2020-03-05 16:13:56 -0600

    Add type RealJsPDF to expose more methods of jsPDF to TypeScript
    The module @types/jspdf tells TypeScript what methods objects of class jsPDF have. However, it is awfully conservative with which methods it chooses to expose. Rather than expose all of jspdf's public API, it chooses only to expose those public API methods that are documented. Unfortunately, the documentation for jspdf is incomplete; quite a few public methods are undocumented, and hence unexposed by @types/jspdf.
    
    This commit declares a new type, RealJsPDF, that exposes more public methods of the jsPDF class. (Not all of them—just the ones we need right now.)
    
    Addresses #10.

--- client/src/app/pdf.service.ts
+++ client/src/app/pdf.service.ts
@@ -5,6 +5,37 @@ import { environment } from '../environments/environment';
 import { map } from 'rxjs/operators';
 import * as jsPDF from 'jspdf';
 
+/**
+ * This is the jsPDF type, but with more of its methods exposed.
+ *
+ * jspdf, the library, clearly distinguishes between its public API and its
+ * private, internal API.
+ *
+ * Now, the @types/jspdf module, which provides type declarations for using
+ * jspdf in TypeScript, chooses not to expose methods of the jsPDF class
+ * that are part of the public API, but that don't have any documentation.
+ *
+ * This is the wrong decision; those methods are designated for public use and
+ * should not be hidden just because their documentation is incomplete.
+ *
+ * So, here, we declare a new TypeScript type that includes all of the methods
+ * declared in @types/jspdf, plus certain methods that @types/jspdf hides, but
+ * that we use here anyway.
+ */
+export type RealJsPDF = jsPDF & {
+  getNumberOfPages: () => number,
+  getCurrentPageInfo: () => {
+    pageContext: {
+      mediaBox: {
+        bottomLeftX: number,
+        bottomLeftY: number,
+        topRightX: number,
+        topRightY: number,
+      },
+    },
+  },
+};
+
 @Injectable()
 export class PDFService {
   constructor() {
@@ -14,14 +45,14 @@ export class PDFService {
    * Returns a jsPDF object with a link to Professor Rachel's
    * DoorBoard viewer page.
    */
-  getPDF(): jsPDF {
+  getPDF(): RealJsPDF {
     const url: string = environment.BASE_URL + '/viewer';
 
     const doc = new jsPDF({
       orientation: 'portrait',
       unit: 'in',
       format: 'letter',
-    });
+    }) as RealJsPDF;
 
     doc.setFontSize(18);
     doc.text('Rachel Johnson\'s DoorBoard', (8.5 / 2), 0.5, { align: 'center' });
