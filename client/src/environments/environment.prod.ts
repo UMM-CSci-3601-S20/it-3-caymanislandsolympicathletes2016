41fea34f0dffb26d5ba1ce9042727c054caddc5c
Joe Moonan Walbran <56209343+helloworld12321@users.noreply.github.com>
2020-03-06 15:46:15 -0600

    Add production URL to the environment
    Now that we have our droplet, we know where the project will live when it is deployed.

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   API_URL: '/api/',
   // TODO: when we get a droplet, put its IP address here.
-  BASE_URL: '(tbd)',
+  BASE_URL: 'https://64.227.25.112.nip.io',
 };
