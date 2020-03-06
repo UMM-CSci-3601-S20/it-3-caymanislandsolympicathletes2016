662a5676c33e3b47989208d1233a84554219f5c2
Joe Walbran <walbr037@morris.umn.edu>
2020-03-05 21:45:32 -0600

    Fix LGTM errors
    Three unused imports, and also, there were two type="button" attributes on the same button 🙃
    
    Addresses #10

--- client/src/app/pdf.service.ts
+++ client/src/app/pdf.service.ts
@@ -1,8 +1,5 @@
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
-import { map } from 'rxjs/operators';
 import * as jsPDF from 'jspdf';
 
 /**
