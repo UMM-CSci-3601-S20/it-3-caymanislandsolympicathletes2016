57018ddd398e785822e4d26793789f1626c291be
Joe Walbran <walbr037@morris.umn.edu>
2020-03-05 18:32:40 -0600

    Test the "GENERATE PDF" button on the home page
    Using the MockPDFComponent we just defined.
    
    There are no end-to-end tests for it yet, however.
    
    Addresses #10.

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -3,6 +3,8 @@ import {HomeComponent} from './home.component';
 import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
+import { PDFService } from '../pdf.service';
+import { MockPDFService } from 'src/testing/pdf.service.mock';
 
 describe('Home', () => {
 
@@ -10,11 +12,15 @@ describe('Home', () => {
   let fixture: ComponentFixture<HomeComponent>;
   let de: DebugElement;
   let el: HTMLElement;
+  let mockPDFService: MockPDFService;
 
   beforeEach(() => {
+    mockPDFService = new MockPDFService();
+
     TestBed.configureTestingModule({
       imports: [MatCardModule],
       declarations: [HomeComponent], // declare the test component
+      providers: [{provide: PDFService, useValue: mockPDFService}],
     });
 
     fixture = TestBed.createComponent(HomeComponent);
@@ -22,7 +28,21 @@ describe('Home', () => {
     component = fixture.componentInstance; // BannerComponent test instance
 
     // query for the link (<a> tag) by CSS element selector
-    de = fixture.debugElement.query(By.css('.home-card'));
+    de = fixture.debugElement.query(By.css('#generate-pdf-button'));
     el = de.nativeElement;
+  });
+
+  describe('the savePDF() method', () => {
+    it('Gets a pdf document from PDFService and calls .save() on it', () => {
+      component.savePDF();
+      expect(mockPDFService.doc.save).toHaveBeenCalled();
+    });
+  });
+
+  describe('the GENERATE PDF button', () => {
+    it('Gets a pdf document from PDFService and calls .save() on it', () => {
+      el.click();
+      expect(mockPDFService.doc.save).toHaveBeenCalled();
+    });
   });
 });
