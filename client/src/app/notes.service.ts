fe96e58e28a0e6cbe37d8ab206090febe897a182
Erik <eriktr22@gmail.com>
2020-03-08 15:13:45 -0500

    Add addNote() method to note service

--- client/src/app/notes.service.ts
+++ client/src/app/notes.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { Note } from './note';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +17,9 @@ export class NotesService {
 
   getNotes() {
     return this.httpClient.get<Note[]>(this.noteUrl);
+  }
+
+  addNote(newNote: Note): Observable<string> {
+    return this.httpClient.post<{id: string}>(this.noteUrl + '/new', newNote).pipe(map(res => res.id));
   }
 }
