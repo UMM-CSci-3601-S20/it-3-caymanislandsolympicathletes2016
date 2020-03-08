062c23a4d1a9078b39610b12be08721da9de7c69
Erik <eriktr22@gmail.com>
2020-03-08 15:49:55 -0500

    Fix endpoint

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -41,7 +41,7 @@ public class Server {
     server.get("api/notes", noteController::getNotes);
 
     // Add new note
-    server.post("api/new", noteController::addNote);
+    server.post("api/notes/new", noteController::addNote);
 
     server.exception(Exception.class, (e, ctx) -> {
       ctx.status(500);
