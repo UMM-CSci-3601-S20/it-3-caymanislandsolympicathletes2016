f222a9feda8255b240d50ddbdd7772f7a9d8e3bf
Erik <eriktr22@gmail.com>
2020-03-08 15:01:48 -0500

    Add routing for addNoteComponent

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AddNoteComponent } from './add/add-note.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'add', component: AddNoteComponent},
 ];
 
 @NgModule({
