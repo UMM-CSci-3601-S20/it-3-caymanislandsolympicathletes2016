17ef2cef5bfeaa128bd84489f732d8abd6d8f8de
Joe Walbran <walbr037@morris.umn.edu>
2020-03-10 13:46:55 -0500

    Implement NoteService.deleteNote
    This should make all the tests pass.
    
    Furthers #8.

--- client/src/app/notes.service.ts
+++ client/src/app/notes.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { Note } from './note';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +18,23 @@ export class NotesService {
 
   getNotes() {
     return this.httpClient.get<Note[]>(this.noteUrl);
+  }
+
+  /**
+   * Delete a note by ID from the database.
+   *
+   * @return true if the note was deleted, and false if the note wasn't
+   *   deleted (eg, if the ID you gave didn't correspond to a note in the
+   *   database.)
+   *
+   * Usually, you can just ignore the return value.
+   */
+  deleteNote(id: string): Observable<boolean> {
+    type DeleteResponse = 'deleted' | 'nothing deleted';
+
+    const response =
+      this.httpClient.delete<DeleteResponse>(this.noteUrl + '/' + encodeURI(id));
+
+    return response.pipe(map(theResponse => theResponse === 'deleted'));
   }
 }
