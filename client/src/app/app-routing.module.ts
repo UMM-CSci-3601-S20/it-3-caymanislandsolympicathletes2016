b0148250142946678b48cb6969d12de65c315350
Cookie Vang <vang2660@morris.umn.edu>
2020-03-28 15:31:46 -0500

    Add routing for owner component
    This doesn't fully work yet, we're working on getting the server side set up

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { ViewerPageComponent } from './viewer-page/viewer-page.component';
 import { AddNoteComponent } from './add/add-note.component';
 import { EditComponent } from './edit/edit.component';
+import { OwnerComponent } from './owner/owner.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'new', component: AddNoteComponent},
   {path: 'viewers', component: ViewerPageComponent},
-  {path: 'edit/:id', component: EditComponent}
+  {path: 'edit/:id', component: EditComponent},
+  {path: 'owner/:id/posts', component: OwnerComponent}
 ];
 
 @NgModule({
