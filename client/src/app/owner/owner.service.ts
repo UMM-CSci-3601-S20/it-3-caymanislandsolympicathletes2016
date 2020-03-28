121d3e9a8a240eb62756fec105f7c78d425f91f8
Cookie Vang <vang2660@morris.umn.edu>
2020-03-28 13:38:31 -0500

    Add service for owner and tests

---
+++ client/src/app/owner/owner.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Owner } from './owner';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class OwnerService {
+  readonly ownerUrl: string = environment.API_URL + 'owners';
+
+  constructor(private httpClient: HttpClient) {
+  }
+  getOwnerById(id: string): Observable<Owner> {
+    return this.httpClient.get<Owner>(this.ownerUrl + '/' + id);
+  }
+
+
+  addOwner(newOwner: Owner): Observable<string> {
+    // Send post request to add a new owner with the owner data as the body.
+    return this.httpClient.post<{ id: string }>(this.ownerUrl + '/new', newOwner).pipe(map(res => res.id));
+  }
+}
