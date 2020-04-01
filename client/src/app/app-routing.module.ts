3086aa9c9bb59b16d6a7e1e1d14918bedaaaf824
Cookie Vang <vang2660@morris.umn.edu>
2020-03-31 23:24:54 -0500

    Take out home component
    Taking out another reference to the home component which is now owner component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+
 import { ViewerPageComponent } from './viewer-page/viewer-page.component';
 import { AddNoteComponent } from './add/add-note.component';
 import { EditComponent } from './edit/edit.component';
@@ -8,7 +8,7 @@ import { OwnerComponent } from './owner/owner.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+
   {path: 'new', component: AddNoteComponent},
   // {path: '', component: LoginComponent}
   // {path: ':owner', component: OwnerComponent}
