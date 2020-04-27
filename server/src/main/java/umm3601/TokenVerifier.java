55bfa581d545e6755a774213348a3bf11496f7a9
Erik <eriktr22@gmail.com>
2020-04-26 23:02:24 -0500

    Add some describing the methods within TokenVerifier

--- server/src/main/java/umm3601/TokenVerifier.java
+++ server/src/main/java/umm3601/TokenVerifier.java
@@ -23,8 +23,14 @@ public class TokenVerifier {
   public TokenVerifier() {
   }
 
-  // https://community.auth0.com/t/verify-jwt-token-received-from-auth0/35581/4
-  public boolean verifyToken(Context ctx) throws InterruptedException {
+  /**
+   * Given a context containing an Access JWT, verify said token
+   * Based on the code from https://community.auth0.com/t/verify-jwt-token-received-from-auth0/35581/4
+   * @param ctx the context containing the JWT
+   * @return true if the token is successfully verified
+   * Sets context status to 400 if there is an error
+   */
+  public boolean verifyToken(Context ctx) {
     String token = ctx.header("Authorization").replace("Bearer ", "");
     JwkProvider provider = new UrlJwkProvider("https://dev-h60mw6th.auth0.com/");
     try {
@@ -52,6 +58,12 @@ public class TokenVerifier {
     return false;
   }
 
+  /**
+   * Retrieve the subject from a context containing an Access JWT
+   * Note: The method assumes that the context has been verified
+   * @param ctx the context containing the JWT
+   * @return the subject of the JWT as a string
+   */
   public String getSubFromToken(Context ctx) {
     String token = ctx.header("Authorization").replace("Bearer ", "");
     String tokenAsString = null;
@@ -67,6 +79,11 @@ public class TokenVerifier {
     return tokenAsString;
   }
 
+  /**
+   * Calls Auth0 to get a user's info
+   * @param ctx a context containing an Access JWT given to the user who's info is desired
+   * @return the info as a string
+   */
   public String getUserInfo(Context ctx) {
     String token = ctx.header("Authorization");
 
@@ -75,6 +92,11 @@ public class TokenVerifier {
     return userInfo;
   }
 
+  /**
+   * Parses out the nickname from a given user's info
+   * @param userInfo the user's info as a string
+   * @return the user's x500 as a string
+   */
   public String getNewOwnerx500(String userInfo) {
 
     // Pull the x500 out of the body, there's definitely a better way to do this, but idk how
@@ -91,6 +113,11 @@ public class TokenVerifier {
     return x500;
   }
 
+  /**
+   * Parses out the subject from a given user's info
+   * @param userInfo the user's info as a string
+   * @return the sub as a string
+   */
   public String getNewOwnerSub(String userInfo) {
 
     System.err.println(userInfo);
