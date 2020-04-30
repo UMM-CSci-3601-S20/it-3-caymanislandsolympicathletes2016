9031e72ada57c70de4ff4956c48b0ebecb61c0a8
AJ <otten099@morris.umn.edu>
2020-04-30 18:28:18 -0500

    fix reloading page in auth component issue #5

--- client/src/app/authentication/auth.guard.ts
+++ client/src/app/authentication/auth.guard.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, iif, of } from 'rxjs';
 import { AuthService } from './auth.service';
-import { tap } from 'rxjs/operators';
+import { tap, concatMap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +11,19 @@ export class AuthGuard implements CanActivate {
 
   constructor(private auth: AuthService) {}
 
-  canActivate(
+  /*canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean|UrlTree> | boolean {
     return this.auth.isAuthenticated$.pipe(
-      tap(loggedIn => {
-        if (!loggedIn) {
-          this.auth.login();
-        }
-      })
-    );
-  }
+      concatMap(_ => this.auth.handleAuthCallback()),
+       concatMap(result => iif(() => result.loggedIn, of(true), this.auth.login(state.url).pipe(map(_ => false)))));
+  }*/
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.auth.isAuthenticated$.pipe(
+      concatMap(_ => this.auth.handleAuthCallback()),
+      concatMap(result => iif(() => result.loggedIn, of(true), this.auth.login(state.url).pipe(map(_ => false)))));
+}
 
 }
