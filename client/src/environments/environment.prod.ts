3791b5cf846af55eb205925c27e222265b54341c
Kyle Fluto <scv.stack@gmail.com>
2020-05-04 21:21:33 -0500

    Change BASE_URL to point to the new droplet

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_URL: '/api/',
-  BASE_URL: 'https://45.55.52.136.nip.io',
+  BASE_URL: 'https://167.99.55.165.nip.io',
   AUTH_DOMAIN: 'dev-h60mw6th.auth0.com',
   AUTH_CLIENT_ID: 'T2q1s6QPYWzErue4P7DjzjJDBc2mvZTK',
   AUTH_API_DOMAIN: 'https://doorboard.com/api/owners',
